6922834d94dc87ca3274325868ef831e422c4817
tnagorra <weathermist@gmail.com>
2021-07-26 16:56:06 +0545

    Add config for hcaptcha

--- app/Base/public-configs/googleAnalytics.ts
+++ app/Base/public-configs/googleAnalytics.ts
@@ -1,11 +1,17 @@
 import { InitializeOptions } from 'react-ga';
+import {
+    isBeta,
+    isAlpha,
+    isNightly,
+    isDev,
+} from './env';
 
-export const trackingId = isBeta
+export const trackingId = isBeta || isAlpha || isNightly
     ? 'UA-112330910-1'
     : 'UA-112330910-2';
 
 export const gaConfig: InitializeOptions = {
-    debug: isDev || isNightly || isAlpha,
+    debug: isDev,
     testMode: isDev,
     gaOptions: {
         userId: undefined,
